b46f2d60274cb49e2aadd765260a3b5ff95c80f7
João Kiakumbo <jkiakumbo@icloud.com>
2025-07-25 06:54:53 +0100

    first commit

---
+++ src/main/java/dev/jkiakumbo/feature_flag_system/FeatureFlagSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.jkiakumbo.feature_flag_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jkiakumbo/feature_flag_system/FeatureFlagSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jkiakumbo.feature_flag_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureFlagSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
