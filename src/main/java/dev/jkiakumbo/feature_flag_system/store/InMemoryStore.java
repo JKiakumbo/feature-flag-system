96cada13dfcbdeb84392822b376064a7d95e8eec
João Kiakumbo <jkiakumbo@icloud.com>
2025-07-25 07:18:03 +0100

    Implement InMemory store

---
+++ src/main/java/dev/jkiakumbo/feature_flag_system/store/InMemoryStore.java
@@ -0,0 +1,23 @@
+package dev.jkiakumbo.feature_flag_system.store;
+
+import dev.jkiakumbo.feature_flag_system.model.FeatureFlag;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryStore {
+    private Map<String, FeatureFlag> flags = new ConcurrentHashMap<>();
+
+    public FeatureFlag getFlag(String name) {
+        return flags.get(name);
+    }
+
+    public void setFlag(String name, FeatureFlag flag) {
+        flags.put(name, flag);
+    }
+
+    public Collection<FeatureFlag> getAllFlags() {
+        return flags.values();
+    }
+}
